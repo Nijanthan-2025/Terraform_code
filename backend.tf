fd20f311f04e4d9c8c9ec113988655da928861fd
devapriyan1902 <nijanthanchamp@gmail.com>
2025-05-02 09:52:51 +0530

    adding backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "S3" {
+      bucket = "my-terraform-state-bucket"
+      key = "prod/ec2-app-db/terraform.tfstate"
+      region = "us-east-1"
+      dynamodb_table = "terraform-lock"
+      encrypt = true
+    }
+}
